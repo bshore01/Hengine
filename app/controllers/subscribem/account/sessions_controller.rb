6b797f5998fe1a2a3e58891a2576e7195742f8f4
bshore01 <github email address>
2015-08-11 23:56:29 -0400

    adding auth

---
+++ app/controllers/subscribem/account/sessions_controller.rb
@@ -0,0 +1,14 @@
+module Subscribem
+	class Account::SessionsController < Subscribem::ApplicationController
+		def new
+			@user = User.new
+		end
+
+		def create
+			if env["warden"].authenticate(:scope => :user)
+				flash[:notice] = "You are now signed in."
+				redirect_to root_path
+			end
+		end
+	end
+end
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -12,7 +12,7 @@ require "rails_helper"
 		 success_message = "Your account has been successfully created."
 		 expect(page).to have_content(success_message)
 		 expect(page).to have_content("Signed in as subscribem@example.com")
-		 expect(page.current_url).to eq("http://test.example.com/subscribem/")
+		 expect(page.current_url).to eq("http://test.example.com/")
  	 end
  	 scenario "Ensure subdomain uniqueness" do
 		 Subscribem::Account.create!(:subdomain => "test", :name => "Test")
@@ -24,8 +24,34 @@ require "rails_helper"
 		 fill_in "Password", :with => "password"
 		 fill_in "Password confirmation", :with => 'password'
 		 click_button "Create Account"
-		 expect(page.current_url).to eq("http://www.example.com/subscribem/accounts")
+		 expect(page.current_url).to eq("http://www.example.com/accounts")
 		 expect(page).to have_content("Sorry, your account could not be created.")
 		 expect(page).to have_content("Subdomain has already been taken")
+	end
+	scenario "Subdomain with restricted name" do
+		visit subscribem.root_path
+		click_link "Account Sign Up"
+		fill_in "Name", :with => "Test"
+		fill_in "Subdomain", :with => "Admin"
+		fill_in "Email", :with => "subscribem@example.com"
+		fill_in "Password", :with => "password"
+		fill_in "Password confirmation", :with => "password"
+		click_button "Create Account"
+		expect(page.current_url).to eq("http://www.example.com/accounts")
+		expect(page).to have_content("Sorry, your account could not be created.")
+		expect(page).to have_content("Subdomain is not allowed. Please choose another subdomain.")
+	end
+	scenario "Subdomain with invalid name" do
+		visit subscribem.root_path
+		click_link "Account Sign Up"
+		fill_in "Name", :with => "Test"
+		fill_in "Subdomain", :with => "<admin>"
+		fill_in "Email", :with => "subscribem@example.com"
+		fill_in "Password", :with => "password"
+		fill_in "Password confirmation", :with => "password"
+		click_button "Create Account"
+		expect(page.current_url).to eq("http://www.example.com/accounts")
+		expect(page).to have_content("Sorry, your account could not be created.")
+		expect(page).to have_content("Subdomain is not allowed. Please choose another subdomain.")
 	end
   end
