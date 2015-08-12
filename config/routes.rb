fb6315853b0d9e4b8529b649558ada28fef92948
bshore01 <github email address>
2015-08-11 22:20:21 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Subscribem::Engine.routes.draw do
+
+	get "/sign_up", :to => "accounts#new", :as => :sign_up
+	post "/accounts", :to => "accounts#create", :as => :accounts
+
+	root "dashboard#index"
+end
---
+++ lib/subscribem/engine.rb
@@ -0,0 +1,21 @@
+module Subscribem
+  class Engine < ::Rails::Engine
+  	require 'warden'
+
+  	initializer "subscribem.middleware.warden" do
+		Rails.application.config.middleware.use Warden::Manager do |manager|
+			manager.serialize_into_session do |user|
+				user.id
+			end
+			manager.serialize_from_session do |id|
+				Subscribem::User.find(id)
+			end
+		end
+	end
+
+  	config.generators do |g|
+		g.test_framework :rspec, :view_specs => false
+	end
+    isolate_namespace Subscribem
+  end
+end
